16a2e01eef82bcfdaa22fcd639777aa53dd5dd2a
chaerish <ccyy8432@naver.com>
2023-11-15 18:21:07 +0900

    first commit

---
+++ src/main/java/twotwone/notAlone/NotAloneApplication.java
@@ -0,0 +1,13 @@
+package twotwone.notAlone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotAloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotAloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/twotwone/notAlone/NotAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package twotwone.notAlone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotAloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
